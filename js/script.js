5aff96ef3c9766a525f0e975f19eef5a90af89c1
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-25 18:30:06 -0300

    atS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+const key = '9d72c0eb7954268cae0edba29ee030b3'
+
+async function buscarCidade (cidade) {
+
+  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(res => res.json())
+  
+  console.log(dados)
+}
+
+function cliqueCidade () {
+  const cidade = document.querySelector('.input-cidade').value
+
+  console.log(cidade)
+}
