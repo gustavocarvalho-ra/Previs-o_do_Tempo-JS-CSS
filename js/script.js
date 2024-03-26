0db348c23472b54fe184b3c97334520e53022167
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-25 23:40:44 -0300

    atS2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const key = "9d72c0eb7954268cae0edba29ee030b3"
 
+function colocarDadosNaTela(dados) {
+
+}
+
 async function buscarCidade(cidade) {
 
   const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
-  
+
   console.log(dados)
 }
 
