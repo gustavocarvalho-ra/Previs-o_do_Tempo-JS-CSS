0bb2d9bc2e101b753b9c066cd1df67bd8f84cfab
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-27 00:47:24 -0300

    atS5

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 const key = "9d72c0eb7954268cae0edba29ee030b3"
 
 function colocarDadosNaTela(dados) {
-
+  console.log(dados)
+  document.querySelector('.cidade').innerHTML = "Tempo em " + dados.name
+  document.querySelector('.temp').innerHTML = dados.main.temp
+  document.querySelector('.previsao').innerHTML = dados.weather[0].description
+  document.querySelector('.umidade').innerHTML = dados.main.humidity
+  document.querySelector('.temp').innerHTML = dados.main.temp
 }
 
 async function buscarCidade(cidade) {
 
-  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
+  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
 
-  console.log(dados)
+  colocarDadosNaTela(dados)
 }
 
 
@@ -16,5 +21,4 @@ function cliqueCidade () {
   const cidade = document.querySelector(".input-cidade").value
 
   buscarCidade(cidade)
-  console.log(cidade)
 }
